4bd39db96f96617fb6e064b847b3c7ae62872290
JobaerSiddique <jobaersiddique28me@gmail.com>
2023-01-09 19:47:53 +0600

    ok

---
+++ src/Components/Livetv.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Livetv = () => {
+    return (
+        <div>
+            <h1>This is Live tv site...worker server</h1>
+        </div>
+    );
+};
+
+export default Livetv;
---
+++ src/Components/Radio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Radio = () => {
+    return (
+        <div>
+            <h1>This is Radio site</h1>
+        </div>
+    );
+};
+
+export default Radio;
