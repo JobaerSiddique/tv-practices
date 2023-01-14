357727c3f15ba1915630ab6036bcdc74634ce5e7
JobaerSiddique <jobaersiddique28me@gmail.com>
2023-01-14 18:09:57 +0600

    tt

--- src/Components/TvLink.js
+++ src/Components/TvLink.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TvLink = ({t}) => {
-    const {name,img,link} = t
+  const [play,setPlay]=useState('')  
+  const {name,img,link} = t
+
+  const handleClick = ()=>{
+    const plays = {link}
+    setPlay(plays)
+  }
     return (
         <div>
-            <video src={link}></video>
-            <div onClick={link} className="card w-96 bg-base-100 shadow-xl ">
+            <iframe src={link}></iframe>
+            <div onClick={handleClick} className="card w-96 bg-base-100 shadow-xl ">
   <figure className="px-10 pt-10">
     <img src={img} className="rounded-xl" />
   </figure>
